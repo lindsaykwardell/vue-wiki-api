504a0fba4fcf25e0e0d7f5fcda256694c1484051
Lindasy Wardell <three060@gmail.com>
2021-09-13 12:42:25 -0700

    Adjustments to styling

--- src/App.vue
+++ src/App.vue
@@ -15,14 +15,15 @@ const {
 
 <template>
   <main class="m-auto pt-6">
-    <input v-model="query" @keyup.enter="performSearch" class="border shadow-lg w-1/3 m-auto block" placeholder="Search for pages containing [x]..." />
+    <h1 class="text-3xl text-center pb-3">Wikipedia Search API</h1>
+    <input v-model="query" @keyup.enter="performSearch" class="p-2 border shadow-lg w-1/3 m-auto block" placeholder="Search for pages containing [x]..." />
     <div v-if="status === Status.ERROR">An error occurred</div>
     <div v-for="result in results" :key="result.id" class="flex border-t border-b p-4">
-      <div class="flex-shrink w-32">
+      <div class="flex-shrink w-32 mr-3">
         <img :src="result?.thumbnail?.url" />
       </div>
       <div class="flex-grow">
-        <h1 class="text-2xl">{{ result.title }}</h1>
+        <h2 class="text-2xl">{{ result.title }}</h2>
         <p>{{ result.description }}</p>
       </div>
     </div>
